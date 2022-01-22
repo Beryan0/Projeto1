11d0426a4840105f93160cb8aea49a39ea1139be
Beryan0 <andradevfernando@gmail.com>
2022-01-22 18:38:18 -0300

    Melhora na condição For

--- src/classes/Verification2.cs
+++ src/classes/Verification2.cs
@@ -1,10 +1,11 @@
+
 public static class Verification2
 {
     public static bool NumberVerification(this String input)
     {
         char[] verification1 = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
         int j = 1;
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < verification1.Length; i++)
         {
             bool ver3 = input.Contains(verification1[i]);
             if (ver3 == true)
